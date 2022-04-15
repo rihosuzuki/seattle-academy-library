b3e64047ea62a844bee8cdaffa758678bd48ba83
鈴木里歩 <riho@suzukisatofunoMacBook-Air.local>
2022-04-15 17:56:10 +0900

    修正

--- src/main/java/jp/co/seattle/library/service/UsersService.java
+++ src/main/java/jp/co/seattle/library/service/UsersService.java
@@ -44,7 +44,7 @@ public class UsersService {
      */
     public UserInfo selectUserInfo(String email, String password) {
         // TODO SQL生成
-        String sql = "SELECT";
+        String sql = "";
 
         UserInfo selectedUserInfo = jdbcTemplate.queryForObject(sql, new UserCountRowMapper());
         return selectedUserInfo;
